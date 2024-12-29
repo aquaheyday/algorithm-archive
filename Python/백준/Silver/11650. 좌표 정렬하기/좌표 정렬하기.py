f7901332fcef5fb576774f8bc76ccef0d60ecbdb
Kim Byoung Jun <aquaheyday@gmail.com>
2024-12-29 16:27:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 320 ms, Memory: 46628 KB -BaekjoonHub

---
+++ Python/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = []
+
+for i in range(n):
+    x, y = map(int, input().split())
+    a.append([x, y])
+    
+a.sort()
+
+for x, y in a:
+    print(x, y)
+
+    
+    
