efc1ffef9dfa6ce50776e4f982f4b749793a11e2
Kim Byoung Jun <aquaheyday@gmail.com>
2024-12-29 16:27:20 +0900

    [Silver I] Title: 절댓값 힙, Time: 140 ms, Memory: 40848 KB -BaekjoonHub

---
+++ Python/백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,19 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+h = []
+
+for i in range(n):
+    x = int(input())
+    
+    if x == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(h[0][1])
+            heapq.heappop(h)
+    else:
+        heapq.heappush(h, [abs(x), x])
+
