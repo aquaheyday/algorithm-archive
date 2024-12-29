51d8876e0d665d323c8eacdc4bf69a42770cbacb
Kim Byoung Jun <aquaheyday@gmail.com>
2024-12-29 16:34:28 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, x, k = map(int, input().split())
+
+for i in range(k):
+    a, b = map(int, input().split())
+    
+    if x == a:
+        x = b
+    elif x == b:
+        x = a
+
+print(x)
