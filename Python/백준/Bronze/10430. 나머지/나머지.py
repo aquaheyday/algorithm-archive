0901f7b4ab6f96384d6d4c3433db5a1706f0c3e0
ByoungJun Kim <aquaheyday@gmail.com>
2025-07-24 17:36:18 +0900

    [Bronze V] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/10430. 나머지/나머지.py
+++ Python/백준/Bronze/10430. 나머지/나머지.py
@@ -1,9 +1,6 @@
-import sys
-input = sys.stdin.readline
+a,b,c = map(int, input().split())
 
-a, b, c = map(int, input().split())
-
-print((a + b) % c)
-print(((a % c) + (b % c)) % c)
-print((a * b) % c)
-print(((a % c) * (b % c)) % c)
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
